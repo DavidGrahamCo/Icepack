a2fac13d68263cd5f73ec71653b36cdc96df2eed
DavidGrahamCo <dagr5998@colorado.edu>
2026-09-14 17:39:09 -0600

    minor change

--- runningacrossbarents.sh
+++ runningacrossbarents.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #source /home/dj_gr/miniconda3/etc/profile.d/conda.sh
-module load anaconda
-conda activate icepack
+#module load anaconda
+#conda activate icepack
 DATA_DIR="$(cd ../input/forcing/barents_combined && pwd)"
 cd configuration/scripts/options
 #Run everything from Icepack root
