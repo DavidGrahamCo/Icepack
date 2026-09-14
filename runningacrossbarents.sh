8b64393e70707685a4e008b8247589f60df29d43
DavidGrahamCo <dagr5998@colorado.edu>
2026-09-14 17:26:46 -0600

    uploading barents script

---
+++ runningacrossbarents.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#source /home/dj_gr/miniconda3/etc/profile.d/conda.sh
+module load anaconda
+conda activate icepack
+DATA_DIR="$(cd ../input/Icepack_data/forcing/barents_combined && pwd)"
+cd configuration/scripts/options
+#Run everything from Icepack root
+
+for i in $(seq 1 5); do
+  n=$(printf '%04d' "$i")
+  cat > set_nml.forcing${n} <<EOF
+input_lat       = 1.309
+input_lon       = 0.698132
+atm_data_type   = 'JRA55'
+ocn_data_type   = 'ISPOL'
+data_dir = '${DATA_DIR}'
+atm_data_file   = 'ATM_FORCING_${n}.txt'
+ocn_data_file   = 'OCN_FORCING_${n}.txt'
+ice_ic          = 'none'
+npt             = 105120
+EOF
+done
+
+cd ../../..
+rm -rf curc_icepack_test*
+for i in $(seq 1 5); do
+  n=$(printf '%04d' "$i")
+  ./icepack.setup --case curc_icepack_test${n} --mach conda --env linux -s restcdf,histcdf,ionetcdf,forcing${n}
+  cd curc_icepack_test${n}
+  ./icepack.build
+  ./icepack.submit
+  cd ..
+done
